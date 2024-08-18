7c33a51ba5f67bd9fe294e7943bc9e0a8da04c2b
Sarah Gad <sarahgad298@gmail.com>
2024-08-18 18:08:36 +0300

    pycodestyle

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,7 @@
 """This module is for task n.0"""
 from typing import Tuple
 
+
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """this fucntion returns the atart and the end indexes"""
     start = (page - 1) * page_size
