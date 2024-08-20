b2f31d06cb73459a1e70977bb5813c93a3b0a947
Sarah Gad <sarahgad298@gmail.com>
2024-08-20 06:15:08 +0300

    edit

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,7 +37,7 @@ class Server:
         assert isinstance(page_size, int) and page_size > 0
         self.dataset()
         re_tup = index_range(page, page_size)
-        if (len(self.__dataset[0]) < re_tup[1]):
+        if (len(self.__dataset) < re_tup[1]):
             return []
         else:
             return self.__dataset[re_tup[0]: re_tup[1]]
