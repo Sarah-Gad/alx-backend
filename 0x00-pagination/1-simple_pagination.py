90a3316281602b2170621c41bd48dc23ffe524e9
Sarah Gad <sarahgad298@gmail.com>
2024-08-20 06:45:43 +0300

    10

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,7 +9,7 @@ import csv
 def index_range(page: int, page_size: int) -> Tuple:
     """this fucntion returns the atart and the end indexes"""
     start = (page - 1) * page_size
-    end = start + page_size
+    end = page * page_size
     return (start, end)
 
 
