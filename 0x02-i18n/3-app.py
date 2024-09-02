11b85c402eb9a5cbdbd6994967799b6587867ebd
Sarah Gad <sarahgad298@gmail.com>
2024-09-02 07:12:33 +0300

    task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Task 1 Module"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """This class represents the falsk config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """this fucntion will get the best language match"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def home():
+    """This is the home page"""
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
